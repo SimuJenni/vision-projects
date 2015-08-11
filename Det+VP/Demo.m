76c195ae6213fed9e62f5102212ad60e3906d92b
Simon Jenni <simujenni@students.unibe.ch>
2015-08-11 14:54:49 +0200

    Replaced the image-gradiets by HOG features

--- Det+VP/Demo.m
+++ Det+VP/Demo.m
@@ -1,9 +1,9 @@
-
-car_id = '09';
+run ~/3rd_party_libs/vlfeat-0.9.19/toolbox/vl_setup
+car_id = '01';
 epflDatasetPath = '~/data/epfl-gims08/tripod-seq/';
 
 files = dir([epflDatasetPath 'tripod_seq_' car_id '_*.jpg']);
-factor = 4;
+factor = 2;
 w = 64*factor;
 h = 64*factor;
 N = 2;%*factor;
@@ -11,27 +11,38 @@ W = w*(N+1);
 H = h;
 n = length(files);
 bin = 8;
+cellSize = 8;
+featDim = 31;
 
-rep  = zeros(H,W);
-repbroom  = zeros(H,W);
-filterSize = w/2;
-g_x=fspecial('gaussian',[1 filterSize], bin/2);
-g_x = repmat(g_x, [H, 1]);
+
+wFeat = w/cellSize;
+hFeat = h/cellSize;
+rep  = zeros(H, W, featDim);
+repbroom  = zeros(H/cellSize, W/cellSize, featDim);
+filterSize = wFeat/2;
+g_x=fspecial('gaussian',[1 filterSize], 2);
+g_x = repmat(g_x, [hFeat, 1, 31]);
 
 
 for i = 1 : length(files)
     
-    stride = floor((N/n)*w*(i-1)) + 1;
+%     stride = floor((N/n)*w*(i-1)) + 1;
+    stride = floor((N/n)*wFeat*(i-1)) + 1;
+
     I = im2double(rgb2gray(imread( [epflDatasetPath files(i).name] )));
-    I = imgradient(I);
-    rep( 1:h, stride:stride+w-1) = rep(1:h, stride:stride+w-1) + imresize( I, [h w] );
-    im = imresize( I, [h w] );
+%     Igrad = imgradient(I);
+%     rep( 1:h, stride:stride+w-1) = rep(1:h, stride:stride+w-1) + imresize( Igrad, [h w] );
+%     im = imresize( I, [h w]);
 %     repbroom( 1:h, floor(w/2)+stride:floor(w/2)+stride+bin-1) = ...
 %         repbroom( 1:h, floor(w/2)+stride:floor(w/2)+stride+bin-1)+im(:,floor(w/2)+[1:bin]);
-    seg = im(:,floor(w/2)+[-filterSize/2:filterSize/2-1]).*g_x;
-    repbroom( 1:h, floor(w/2)+stride+[-filterSize/2:filterSize/2-1]) = ...
-        repbroom( 1:h, floor(w/2)+stride+[-filterSize/2:filterSize/2-1])+seg;
-    figure(1), imshow(repbroom,[]);
+    
+    im = single(imresize( I, [h w] ));
+    hog = vl_hog(im, cellSize) ;
+
+    seg = hog(:,floor(wFeat/2)+[-filterSize/2:filterSize/2-1],:).*g_x;
+    repbroom( 1:hFeat, floor(wFeat/2)+stride+[-filterSize/2:filterSize/2-1],:) = ...
+        repbroom( 1:hFeat, floor(wFeat/2)+stride+[-filterSize/2:filterSize/2-1],:)+seg;
+    figure(1), imshow(vl_hog('render', single(repbroom)),[]);
     pause(.001);
 end
 
