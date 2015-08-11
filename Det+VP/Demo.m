efefd890b5b5ba203fc15d70d8d20c111b5613d6
Simon Jenni <simujenni@students.unibe.ch>
2015-08-11 13:46:32 +0200

    Added a smoothing filter before the stitching together of the image-segments

--- Det+VP/Demo.m
+++ Det+VP/Demo.m
@@ -1,18 +1,23 @@
 
-car_id = '01';
+car_id = '09';
 epflDatasetPath = '~/data/epfl-gims08/tripod-seq/';
 
 files = dir([epflDatasetPath 'tripod_seq_' car_id '_*.jpg']);
-factor = 6;
+factor = 4;
 w = 64*factor;
 h = 64*factor;
 N = 2;%*factor;
 W = w*(N+1);
 H = h;
 n = length(files);
+bin = 8;
 
 rep  = zeros(H,W);
 repbroom  = zeros(H,W);
+filterSize = w/2;
+g_x=fspecial('gaussian',[1 filterSize], bin/2);
+g_x = repmat(g_x, [H, 1]);
+
 
 for i = 1 : length(files)
     
@@ -21,11 +26,14 @@ for i = 1 : length(files)
     I = imgradient(I);
     rep( 1:h, stride:stride+w-1) = rep(1:h, stride:stride+w-1) + imresize( I, [h w] );
     im = imresize( I, [h w] );
-    repbroom( 1:h, floor(w/2)+stride:floor(w/2)+stride+10-1) = im(:,floor(w/2)+[1:10]);
-    
+%     repbroom( 1:h, floor(w/2)+stride:floor(w/2)+stride+bin-1) = ...
+%         repbroom( 1:h, floor(w/2)+stride:floor(w/2)+stride+bin-1)+im(:,floor(w/2)+[1:bin]);
+    seg = im(:,floor(w/2)+[-filterSize/2:filterSize/2-1]).*g_x;
+    repbroom( 1:h, floor(w/2)+stride+[-filterSize/2:filterSize/2-1]) = ...
+        repbroom( 1:h, floor(w/2)+stride+[-filterSize/2:filterSize/2-1])+seg;
     figure(1), imshow(repbroom,[]);
     pause(.001);
 end
 
-%figure(2), imshow(repbroom,[]);
+% figure(2), imshow(repbroom,[]);
 
